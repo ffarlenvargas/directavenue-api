74ff272d05b77434b6bce69d9577cb5a4a996920
ff_arlenvargas <arlen@firstfactory.com>
2024-11-12 07:21:30 -0600

    DA-8 Fix test

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,12 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
 
 describe('AppController', () => {
   let appController: AppController;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
+      imports: [PrismaModule],
       controllers: [AppController],
       providers: [AppService],
     }).compile();
@@ -15,8 +17,13 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Hello World!"', async () => {
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        send: jest.fn(),
+      };
+      await appController.getHello(res);
+      expect(res.status).toHaveBeenCalledWith(200);
     });
   });
 });
