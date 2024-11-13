babe8615396968595e750544cca07412560a0a13
ff_arlenvargas <arlen@firstfactory.com>
2024-11-13 10:31:49 -0600

    DA-8 Update mock information

--- src/api/advertiser/advertiser.resolver.spec.ts
+++ src/api/advertiser/advertiser.resolver.spec.ts
@@ -5,16 +5,14 @@ import { Advertiser } from './dto/advertiser.dto';
 
 const mockAdvertiser: Advertiser = {
   recId: 1,
-  advtId: 1166,
-  address:
-    '220 West Germantown Pike Suite 250,45-5553972,Plymouth Meeting,PA,US',
-  advtName: 'AdaptHealth LLC',
-  brandName: 'ActivStyle',
-  campName: 'Q1 - 2024 Media Buys',
-  contact: 'Benjamin Helfinstein',
-  email: 'bhelfinstein@adapthealth.com',
-  notes:
-    'THIS IS A CREATIVE SPECIFIC CONTRACT.  YOU MUST REFERENCE THE ACCESS AND DESCRIPTION LISTED ABOVE WHEN ENTERING THE BRAND FOR THIS ORDER Alterations to this order are considered null unless approved and confirmed by the media buyer All Schedules are subject to 48 hours cancellation No Make Good w/o Prior Approval All Schedules require Pre & Post Logs 20 min. separation required on all spots and 60 min. separation required on all spots $500 or more. Please confirm receipt of this order',
+  advtId: 2,
+  address: 'test',
+  advtName: 'test',
+  brandName: 'test',
+  campName: 'test',
+  contact: 'arlen',
+  email: 'arlen@yopmail.con',
+  notes: 'Note',
 };
 
 const advertiserServiceMock = {
